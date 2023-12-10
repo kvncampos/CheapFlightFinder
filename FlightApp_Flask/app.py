b315cca825ad24beb55a1a6f41c343b01cdb9993
kvncampos <kvnelpdfw@hotmail.com>
2023-12-10 13:44:05 -0600

    added delete button

--- FlightApp_Flask/app.py
+++ FlightApp_Flask/app.py
@@ -1,7 +1,7 @@
-import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
 import json
+import os
 
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
@@ -16,7 +16,7 @@ def create_file_if_not_exists(file_path, default_content):
             json.dump(default_content, json_file)
 
 # Check if the directory and file exist, create them if not
-emails_directory = 'flight_app/emails'
+emails_directory = 'emails'
 emails_file_path = os.path.join(emails_directory, 'user_emails.json')
 create_file_if_not_exists(emails_directory, {})
 create_file_if_not_exists(emails_file_path, {})
@@ -27,7 +27,9 @@ def home():
 
     if request.method == 'POST':
         email = request.form.get('email')
-        if email:
+        action = request.form.get('action')
+
+        if action == 'register' and email:
             # Categorize the email
             email_category = 'GMAIL' if 'gmail' in email.casefold() else 'INVALID'
 
@@ -38,11 +40,17 @@ def home():
             with open(emails_file_path, 'w') as json_file:
                 json.dump(user_emails, json_file)
 
-            # Debugging: Print file path and message
-            app.logger.info("File Path: %s", emails_file_path)
-            app.logger.info("Emails: %s", user_emails)
-
             message = 'Registration successful!'
+
+        elif action == 'delete' and email in user_emails:
+            # Remove the email entry
+            del user_emails[email]
+
+            # Save the updated emails to a JSON file
+            with open(emails_file_path, 'w') as json_file:
+                json.dump(user_emails, json_file)
+
+            message = 'Unsubscribe successful!'
 
     return render_template('index.html', message=message)
 
